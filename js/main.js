2b15abff4f7cae8dbb3181f6e5f07ed3006ab818
AlessandroDiNardo <dinardoalessandro98@gmail.com>
2022-11-25 14:44:07 +0100

    esercizio completato

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return{
+            title:'HELLO GUYSSSSS!!!!!',
+            VueImg:'./img/spallettone.jpg',
+            message:'FOZZA NAPOLI!!!'
+        }
+    }
+}).mount('#app')
